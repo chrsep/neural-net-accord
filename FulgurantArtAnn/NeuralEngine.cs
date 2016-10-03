c4c5bfe4bd3448a1638c7cbaca597cbd9acb3f02
chrisandoep <chrisep8@gmail.com>
2016-10-03 19:54:10 +0700

    added network and trainer

--- FulgurantArtAnn/NeuralEngine.cs
+++ FulgurantArtAnn/NeuralEngine.cs
@@ -1,16 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AForge.Neuro;
+using AForge.Neuro.Learning;
 
 namespace FulgurantArtAnn
 {
     class NeuralEngine 
     {
         private static NeuralEngine _instance;
+        private SOMLearning _somTrainer;
+        private BackPropagationLearning _bpTrainer;
 
-        private NeuralEngine(){}
+        private NeuralEngine()
+        {
+            DistanceNetwork distanceNetwork;
+            ActivationNetwork activationNetwork;
+
+            try
+            {
+                activationNetwork = Network.Load("BPNNBrain.net") as ActivationNetwork;
+                distanceNetwork = Network.Load("SOMBrain.net") as DistanceNetwork;
+            }
+            catch (FileNotFoundException e)
+            {
+                activationNetwork = new ActivationNetwork(new SigmoidFunction(), 100, 100);
+                distanceNetwork = new DistanceNetwork(100, 100);
+            }
+            
+
+            _somTrainer = new SOMLearning(distanceNetwork);
+            _bpTrainer = new BackPropagationLearning(activationNetwork);
+        }
 
         public static NeuralEngine Instance => _instance ?? (_instance = new NeuralEngine());
     }
