c253045c29ca2eb8ce09c3897879f095c91a4c5c
yuichikategawa <michael_z96@live.com>
2016-10-04 08:22:28 +0700

    Revert "Revert "Add Art Form, Browse Image and Show Image (The file name sill missing though)""
    This reverts commit 0770e3f0bb948cbcf61186c398c488cd2b23537a.

--- FulgurantArtAnn/AddArtForm.Designer.cs
+++ FulgurantArtAnn/AddArtForm.Designer.cs
@@ -29,10 +29,132 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AddNewArt = new System.Windows.Forms.Button();
+            this.SubmitArtBtn = new System.Windows.Forms.Button();
+            this.BackLblLink = new System.Windows.Forms.LinkLabel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ArtStudioTxtBox = new System.Windows.Forms.TextBox();
+            this.ViewArt = new System.Windows.Forms.ListView();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add New Art";
+            // 
+            // AddNewArt
+            // 
+            this.AddNewArt.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.AddNewArt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddNewArt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.AddNewArt.Location = new System.Drawing.Point(12, 192);
+            this.AddNewArt.Name = "AddNewArt";
+            this.AddNewArt.Size = new System.Drawing.Size(120, 23);
+            this.AddNewArt.TabIndex = 1;
+            this.AddNewArt.Text = "Add New";
+            this.AddNewArt.UseVisualStyleBackColor = false;
+            this.AddNewArt.Click += new System.EventHandler(this.AddNewArt_Click);
+            // 
+            // SubmitArtBtn
+            // 
+            this.SubmitArtBtn.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.SubmitArtBtn.ForeColor = System.Drawing.Color.DarkOrange;
+            this.SubmitArtBtn.Location = new System.Drawing.Point(12, 246);
+            this.SubmitArtBtn.Name = "SubmitArtBtn";
+            this.SubmitArtBtn.Size = new System.Drawing.Size(308, 50);
+            this.SubmitArtBtn.TabIndex = 2;
+            this.SubmitArtBtn.Text = "Submit Art";
+            this.SubmitArtBtn.UseVisualStyleBackColor = false;
+            // 
+            // BackLblLink
+            // 
+            this.BackLblLink.AutoSize = true;
+            this.BackLblLink.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.BackLblLink.Location = new System.Drawing.Point(288, 323);
+            this.BackLblLink.Name = "BackLblLink";
+            this.BackLblLink.Size = new System.Drawing.Size(32, 13);
+            this.BackLblLink.TabIndex = 3;
+            this.BackLblLink.TabStop = true;
+            this.BackLblLink.Text = "Back";
+            this.BackLblLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.BackLblLink_LinkClicked);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.comboBox1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(144, 194);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(176, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "Add New Category";
+            // 
+            // ArtStudioTxtBox
+            // 
+            this.ArtStudioTxtBox.ForeColor = System.Drawing.Color.DarkOrange;
+            this.ArtStudioTxtBox.Location = new System.Drawing.Point(12, 221);
+            this.ArtStudioTxtBox.Name = "ArtStudioTxtBox";
+            this.ArtStudioTxtBox.Size = new System.Drawing.Size(308, 20);
+            this.ArtStudioTxtBox.TabIndex = 5;
+            this.ArtStudioTxtBox.Text = "Art Studio";
+            this.ArtStudioTxtBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ViewArt
+            // 
+            this.ViewArt.Location = new System.Drawing.Point(12, 37);
+            this.ViewArt.Name = "ViewArt";
+            this.ViewArt.Size = new System.Drawing.Size(308, 149);
+            this.ViewArt.TabIndex = 6;
+            this.ViewArt.UseCompatibleStateImageBehavior = false;
+            this.ViewArt.View = System.Windows.Forms.View.SmallIcon;
+            this.ViewArt.SelectedIndexChanged += new System.EventHandler(this.ViewArt_SelectedIndexChanged);
+            // 
+            // imageList
+            // 
+            this.imageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // AddArtForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.ClientSize = new System.Drawing.Size(332, 345);
+            this.Controls.Add(this.ViewArt);
+            this.Controls.Add(this.ArtStudioTxtBox);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.BackLblLink);
+            this.Controls.Add(this.SubmitArtBtn);
+            this.Controls.Add(this.AddNewArt);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.HelpButton = true;
+            this.Name = "AddArtForm";
             this.Text = "AddArtForm";
+            this.Load += new System.EventHandler(this.AddArtForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button AddNewArt;
+        private System.Windows.Forms.Button SubmitArtBtn;
+        private System.Windows.Forms.LinkLabel BackLblLink;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox ArtStudioTxtBox;
+        private System.Windows.Forms.ListView ViewArt;
+        private System.Windows.Forms.ImageList imageList;
     }
 }
